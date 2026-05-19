82af2f174fb1819dc87d6ddd15d451efe573d16a
Pedro Matoso <pedroomatoso@gmail.com>
2026-05-19 12:40:54 -0300

    Add: dynamicArray - submission-3

---
+++ Data Structures & Algorithms/dynamicArray/submission-3.ts
@@ -0,0 +1,82 @@
+class DynamicArray {
+    private arr: number[];
+    private capacity: number;
+    private length: number;
+
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity: number) {
+        this.capacity = capacity;
+        this.length = 0;
+        this.arr = new Array(capacity);
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i: number): number {
+        return this.arr[i];
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i: number, n: number): void {
+        this.arr[i] = n;
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n: number): void {
+        if (this.length === this.capacity) {
+            this.resize();
+        }
+        this.arr[this.length] = n;
+        this.length++;
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback(): number {
+        if (this.length > 0) {
+            // soft delete the last element
+            this.length--;
+        }
+
+        return this.arr[this.length];
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize(): void {
+        this.capacity *= 2;
+        const newArr = new Array(this.capacity);
+        for (let i = 0; i < this.length; i++) {
+            newArr[i] = this.arr[i];
+        }
+        this.arr = newArr;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize(): number {
+        return this.length;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity(): number {
+        return this.capacity;
+    }
+}
