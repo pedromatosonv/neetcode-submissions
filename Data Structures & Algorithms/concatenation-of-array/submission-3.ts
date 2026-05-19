d69509c6967f6a983ecf2e932b95eded3ef891ad
Pedro Matoso <pedroomatoso@gmail.com>
2026-05-19 13:01:51 -0300

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums: number[]): number[] {
+        // const ans = Array.from(nums);
+        const ans = [...nums];
+
+        for (let i = 0; i < nums.length; i++) {
+            ans[i + nums.length] = nums[i];
+        }
+
+        return ans;
+    }
+}
