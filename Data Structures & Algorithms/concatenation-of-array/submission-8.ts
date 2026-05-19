b6b7f791ef1aa645b985acd52afda251a8e443ba
Pedro Matoso <pedroomatoso@gmail.com>
2026-05-19 13:16:59 -0300

    Add: concatenation-of-array - submission-8

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-8.ts
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        const ans = new Array(2 * nums.length);
+        for (let i = 0; i < nums.length; i++) {
+            ans[i] = ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
